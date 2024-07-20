c02727beb4b51b9f0eef6add7033db8515b82b05
minii97 <tmdalsqqz@gmail.com>
2024-07-20 15:40:30 +0900

    ✨ Create createRandomNum function and apply bg.js

--- js/global.js
+++ js/global.js
@@ -1,2 +1,9 @@
 const mainContents = document.querySelector('.main-contents')
 const overlay = document.querySelector('.overlay')
+let randomNum
+
+function crateRandomNum(length) {
+  randomNum = Math.floor(Math.random() * length) + 1
+
+  return randomNum
+}
