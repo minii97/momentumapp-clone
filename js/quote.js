440ce6057c5a8302f141714d38f7efe83f9d623f
minii97 <tmdalsqqz@gmail.com>
2024-07-20 15:40:53 +0900

    ✨ Implement random quote

---
+++ js/quote.js
@@ -0,0 +1,28 @@
+fetch('./assets/data/quote.json')
+  .then((res) => res.json())
+  .then((data) => {
+    const author = document.querySelector('.author')
+    const quote = document.querySelector('.quote')
+
+    const quoteLength = 10
+    crateRandomNum(quoteLength - 1)
+
+    author.innerHTML = data[randomNum].author
+    quote.innerHTML = `"${data[randomNum].quote}"`
+
+    const quoteBtn = document.querySelector('.quote-btn')
+
+    quoteBtn.addEventListener('click', () => {
+      const quoteList = document.querySelector('.quote-list')
+      overlay.classList.remove('hide')
+      quoteList.classList.add('is-active')
+
+      overlay.addEventListener('click', () => {
+        overlay.classList.add('hide')
+        quoteList.classList.remove('is-active')
+      })
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
